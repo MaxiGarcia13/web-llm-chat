c1b62269fad3afc5c9ab87ee06528d428edd4c1e
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2025-11-28 22:51:23 +0100

    Refine model size filtering for Microsoft provider

--- src/supports/getModelsForSize.ts
+++ src/supports/getModelsForSize.ts
@@ -1,20 +1,32 @@
 import type { Model } from '@/types';
 
+const microsoft = 'Microsoft';
+
 export function getModelsForSize(models: Array<Model>, size: 'small' | 'medium' | 'large'): Array<Model> {
   if (size === 'small') {
-    return models.filter(model =>
+    return models.filter((model) => {
+      if (model.provider === microsoft) {
+        return model.name?.match(/mini/i);
+      }
       // eslint-disable-next-line regexp/no-super-linear-backtracking, regexp/optimal-quantifier-concatenation
-      model.name?.match(/^.*(?:\d+(?:[._]\d+)?M|[0-2](?:[._]\d+)?B).*$/i),
-    );
+      return model.name?.match(/^.*(?:\d+(?:[._]\d+)?M|[0-2](?:[._]\d+)?B).*$/i);
+    });
   }
 
   if (size === 'medium') {
-    return models.filter(model =>
+    return models.filter((model) => {
+      if (model.provider === microsoft) {
+        return model.name?.match(/vision/i);
+      }
+
       // eslint-disable-next-line regexp/no-useless-non-capturing-group
-      model.name?.match(/^.*(?:[3-7](?:[._]\d+)?B).*$/i),
+      return model.name?.match(/^.*(?:[3-7](?:[._]\d+)?B).*$/i);
+    },
     );
   }
 
-  // eslint-disable-next-line regexp/no-super-linear-backtracking
-  return models.filter(model => model.name?.match(/^.*(?:[89](?:[._]\d+)?B|[1-9]\d+(?:[._]\d+)?B).*$/i));
+  return models.filter((model) => {
+    // eslint-disable-next-line regexp/no-super-linear-backtracking
+    return model.name?.match(/^.*(?:[89](?:[._]\d+)?B|[1-9]\d+(?:[._]\d+)?B).*$/i);
+  });
 }
